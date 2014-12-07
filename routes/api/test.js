9081132ef0c6b98e365fc996d571b93ba6152235
kopytkoProg <196858@student.pwr.edu.pl>
2014-12-07 17:51:43 +0100

    Start

--- routes/api/test.js
+++ routes/api/test.js
@@ -10,7 +10,7 @@ router.get('/', function (req, res) {
     res.send(
         {
             itIsWorking: true,
-            date: new Date()
+            date: new Date() // OK
         }
     );
 });
