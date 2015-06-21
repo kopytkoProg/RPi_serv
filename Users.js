47d9633265eae582b934f3f117469ed74a86ff93
kopytkoProg <196858@student.pwr.edu.pl>
2015-06-21 23:35:03 +0200

    no more sqllite in use

--- Users.js
+++ Users.js
@@ -2,39 +2,39 @@
  * Created by michal on 2014-12-05.
  */
 var crypto = require('crypto');
-var sqlite3 = require('sqlite3').verbose();
+// var sqlite3 = require('sqlite3').verbose();
 
-var database = new sqlite3.Database('mydb.db');
+// var database = new sqlite3.Database('mydb.db');
 
-database.serialize(function ()
-{
-
-    database.run("CREATE TABLE if not exists Users (Username TEXT, Password TEXT)");
-
-    //var stmt = db.prepare("INSERT INTO Users VALUES (?, ?)");
-    //stmt.run(['michal', 'deff10bf1314f391fac984bb204d2175d73e6f2c5de189659554397e9c4afe6a']);
-    //stmt.finalize();
-});
+//database.serialize(function ()
+//{
+//
+//    database.run("CREATE TABLE if not exists Users (Username TEXT, Password TEXT)");
+//
+//    //var stmt = db.prepare("INSERT INTO Users VALUES (?, ?)");
+//    //stmt.run(['michal', 'deff10bf1314f391fac984bb204d2175d73e6f2c5de189659554397e9c4afe6a']);
+//    //stmt.finalize();
+//});
 
 //database.each("SELECT * FROM Users", function (err, row)
 //{
 //    console.log(row.Username + ": " + row.Password);
 //});
 
-database.close();
+//database.close();
 
 
 var Users = function ()
 {
-    var db = new sqlite3.Database('mydb.db');
+    // var db = new sqlite3.Database('mydb.db');
 
 
-    //var users = [
-    //    {
-    //        username: 'michal',
-    //        password: 'deff10bf1314f391fac984bb204d2175d73e6f2c5de189659554397e9c4afe6a'
-    //    }
-    //];
+    var users = [
+        {
+            Username: 'michal',
+            Password: 'deff10bf1314f391fac984bb204d2175d73e6f2c5de189659554397e9c4afe6a'
+        }
+    ];
 
     function hash(txt)
     {
@@ -44,14 +44,8 @@ var Users = function ()
 
     this.findOneUser = function (login, password, callback)
     {
-
-        var users = [];
-
-        db.all("SELECT * FROM Users", function (err, rows)
+        var f = function(users)
         {
-            users = rows
-            //console.log(rows);
-
             var filtered = users.filter(function (e)
             {
                 return e.Username == login && e.Password == hash(password);
@@ -66,10 +60,21 @@ var Users = function ()
             {
                 callback(null);
             }
+        };
 
-        });
+        f(users);
+        //var users = [];
+        //
+        //db.all("SELECT * FROM Users", function (err, rows)
+        //{
+        //    users = rows
+        //    //console.log(rows);
+        //    f(users);
+        //
+        //});
+
     }
 
-}
+};
 
 module.exports = new Users();
